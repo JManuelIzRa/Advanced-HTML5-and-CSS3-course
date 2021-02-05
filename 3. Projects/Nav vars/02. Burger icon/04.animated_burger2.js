4b7e3189f35243360f52fdab158f6f024ae953ce
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-02-05 01:42:52 +0100

    Animated menu

--- 3. Projects/Nav vars/02. Burger icon/04.animated_burger2.js
+++ 3. Projects/Nav vars/02. Burger icon/04.animated_burger2.js
@@ -1,16 +1,22 @@
-/*const menuBtn = document.querySelector('.menu_bttn');
+const menuBtn = document.querySelector('.menu_bttn');
+
+const menuDisplay = document.querySelector(".nav");
+
+const backgroundDisplay = document.querySelector(".menu_bg");
 
 let menuOpen = false;
 
 menuBtn.addEventListener('click',() => {
     if(!menuOpen){
         menuBtn.classList.add('open');
+        menuDisplay.classList.add('open');
+        backgroundDisplay.classList.add('open');
         menuOpen = true;
     }else{
         menuBtn.classList.remove('open');
+        menuDisplay.classList.remove('open');
+        backgroundDisplay.classList.remove('open');
         menuOpen = false;
     }
-});*/
-
-
+});
 
